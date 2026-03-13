b2b51fe0eb04196b63f44feb8a679df4662f829b
Daniel Desira <desiradaniel2007@gmail.com>
2026-03-13 12:55:56 +0100

    Migrate layout and homepage to next.js

---
+++ app/menu.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Link from "next/link";
+import "./menu.css";
+import { useRef } from "react";
+import { Menu as MenuIcon } from "@deemlol/next-icons";
+
+type MenuItem = { url: string; label: string };
+
+export default function Menu() {
+  const menuItems = [
+    { url: "/", label: "Home" },
+    { url: "/scores", label: "Scores" },
+    { url: "/blog", label: "Blog" },
+  ] satisfies MenuItem[];
+
+  const burgerMenuRef = useRef<HTMLDialogElement>(null);
+
+  const menuItemsJsx = menuItems.map(({ url, label }) => (
+    <Link
+      key={url}
+      href={url}
+      className="hover:bg-pink-400 p-2 rounded-sm text-white text-lg font-bold"
+      onClick={() => burgerMenuRef.current?.hidePopover()}
+    >
+      {label}
+    </Link>
+  ));
+
+  return (
+    <>
+      <menu className="md:flex gap-2 items-center hidden">{menuItemsJsx}</menu>
+      <menu className="flex flex-col md:hidden">
+        <button
+          type="button"
+          className="border border-white rounded-sm"
+          popoverTarget="burgerMenu"
+        >
+          <MenuIcon className="w-8 h-8" />
+        </button>
+      </menu>
+      <dialog
+        id="burgerMenu"
+        ref={burgerMenuRef}
+        popover=""
+        className="fixed top-16 bg-primary flex-col gap-10 justify-center items-center w-full rounded-sm opacity-85"
+      >
+        {menuItemsJsx}
+      </dialog>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+    <div className="flex flex-col gap-5">
+      <h2 className="text-3xl font-bold mb-4 text-primary">
+        Welcome to Mission Sea Turtle Nest community site!
+      </h2>
+      <div className="flex flex-col gap-3 items-center">
+        <p className="font-normal text-sm">
+          I have been volunteering with patrols and nest watch on behalf of
+          Nature Trust Malta since 2020, starting with two turtle nests at
+          Għadira. The nests have hatched in September which has brought about
+          concern of flooding and hence rotting of the eggs due to both rainfall
+          and increased wave action. However, the planning of the coordinating
+          team, as well as manual labour such as digging trenches on part of
+          volunteers including myself has undoubtedly resulted in better
+          numbers. Needless to say, the knowledge acquired from this experience
+          and the people I`&apos`ve met through this journey, have inspired me to
+          build this game from a mama turtle`&apos`s perspective.
+        </p>
+        <a
+          href="https://turtle-quest.vercel.app/"
+          target="_blank"
+          className="px-4 py-2 bg-primary hover:bg-pink-400 transition-colors duration-300 text-white font-bold w-fit rounded-sm"
+        >
+          Launch Game
+        </a>
+        <p className="font-normal text-sm">
+          Dedicated to our coordinators Angelique and Haley and countless turtle
+          watch volunteers (past, present and future), including my dear friends
+          Josette Darmenia, Stephen Spiteri, Ana Feder, Sheila Adams, Joanna
+          Pullicino Kent, Rachel Doyle, Roberta Farrugia Debono, Glenn Brough,
+          Joe Conti, Corrine Wood, Liza Sountsova, Kamilla Boross and Liz
+          Gowland-Camilleri. And many others - it`&apos`s impossible to list them all
+          here! These people have selflessly dedicated their time for
+          safeguarding biodiversity and for that I wish to thank them
+          wholeheartedly.
+        </p>
+        <p className="font-normal text-sm">
+          Of course, also dedicated to mama and baby turtles this amazing team,
+          together with the team of professionals from ERA, has helped to
+          safeguard throughout the years. Lots of respect and love to all of
+          those I have so far met in this journey.
+        </p>
+        <p className="font-normal text-sm">
+          Many thanks to those of you have shared their honest feedback, as well
+          as those who simply enjoyed playing. The list of contributors may be
+          accessed&nbsp;
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://github.com/danieldesira/MissionSeaTurtleNest/blob/master/contributors.md"
+            target="__blank"
+            className="underline"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            here.
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          &nbsp; Special thanks goes to Marvic Refalo (educator at NTM and
+          volunteer wildlife rescuer) for her constructive feedback from testing
+          the game.
+        </p>
+        <p className="font-normal text-sm">
+          Hope for many more baby turtles in the years to come!
+        </p>
+      </div>
     </div>
   );
 }
