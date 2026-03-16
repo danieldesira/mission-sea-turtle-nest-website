15c9e49249ce2b42e3bcf3500cc2b562b2e27b8d
Daniel Desira <desiradaniel2007@gmail.com>
2026-03-16 16:28:37 +0100

    attempt disabling raw html wrapper for live debug

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,4 +1,4 @@
-import SafeRawHtmlWrapper from "@/app/common/safe-raw-html-wrapper";
+//import SafeRawHtmlWrapper from "@/app/common/safe-raw-html-wrapper";
 import { formatDate } from "@/app/common/utils";
 import { getSinglePost } from "../services";
 import { PageQueryProps } from "@/app/types";
@@ -18,11 +18,11 @@ export default async function PostPage({ params }: PageQueryProps) {
         <h2 className="text-2xl font-bold">{post.title}</h2>
       </header>
       <article className="flex flex-col p-2 gap-2">
-        <SafeRawHtmlWrapper html={post.content!} />
+        {/* <SafeRawHtmlWrapper html={post.content!} /> */}
         <span className="font-light text-lg">{post.author?.nice_name}</span>
         <span className="font-light text-sm">{formatDate(post.modified!)}</span>
       </article>
-      {/* <CommentSection id={post.ID!} title={post.title!} url={post.URL!} /> */}
+      <CommentSection id={post.ID!} title={post.title!} url={post.URL!} />
     </div>
   );
 }
